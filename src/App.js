741fe4f80ba955de3e8ce309a548b558124f82e8
KevinPercy <kevinpercycl@gmail.com>
2020-04-28 17:36:05 -0500

    Added state, the burguer builder and styles for the ingredients of the burguer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuiler from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <BurgerBuiler/>
+      </Layout>      
     </div>
   );
 }
